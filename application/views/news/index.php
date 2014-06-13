b2c3212dd9e12f42e6dd2e1fac066d131da78fea
Moisés Pérez <mxpxgx@gmail.com>
2014-06-12 18:51:17 -0700

    testing

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -7,3 +7,12 @@
     <p><a href="<?php echo $news_item['slug'] ?>">View article</a></p>
 
 <?php endforeach ?>
+
+<p>
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    $date = new DateTime('2012-05-18');
+    $formattedDate = $date->format(DateTime::ISO8601); 
+    print_r($formattedDate); // 2012-05-18T00:00:00+0000
+?>
+</p>
